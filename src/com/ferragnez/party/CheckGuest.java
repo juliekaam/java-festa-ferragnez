94dca0095a0bc7601acda26c1758275d5caa3fa8
juliekaam <nestlyguiabu@gmail.com>
2023-07-13 16:28:12 +0200

    feragnez complete

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -10,25 +10,30 @@ chiedere all’utente come si chiamaverificare che il nome sia presente nella li
 ma devo usare il metodo equals(): esempio qui)
 lasciarlo entrare o rispedirlo cortesemente da dove è venuto :sunglasses:*/
 
-        // creazione dell'array
+
         Scanner scan= new Scanner(System.in);
+        // creazione dell'array
         String[] guestName= { "Dua Lipa", "Paris Hilton"," Manuel Agnelli", "J-Ax","Francesco Totti",
                 "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
-        String userName;
+        String userName;//stringa per nome invitato
+        //chiedo nome di chi si presenta
         System.out.print("what is your name?: " );
         userName=scan.nextLine();
         int count;
-        boolean present;
+        boolean presentInList = false;
 
+        //cerco se il nome è presente nella lista
         for (int i=0; i<=guestName.length-1; i++) {
-            present= guestName[i].equals(userName);
-
-
-            if(present){
-                System.out.println("you can acess");
-            }else{
-                System.out.println("sorry");
+            if(userName.equals(guestName[i])){
+                presentInList=true;
+                break;
             }
+        }
+        // decido se entra o no
+        if(presentInList){
+            System.out.println("welcome to the feast");
+        }else{
+            System.out.println("sorry ");
         }
 
     }
